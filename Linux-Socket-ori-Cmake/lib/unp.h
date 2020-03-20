d000ffb540e9a52f0db041ab908a10f7a2d66bd5
lqcone <lqcone1@163.com>
2020-03-20 16:27:12 +0800

    hello

--- Linux-Socket-ori-Cmake/lib/unp.h
+++ Linux-Socket-ori-Cmake/lib/unp.h
@@ -1,13 +1,37 @@
 #include<stdio.h>
 #include<errno.h>
 #include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stddef.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<fcntl.h>
+#include<float.h>
 
 
-
+#define LISTENQ 1024
 
 #define MAXLINE 4069      //文本行最大长度
 
 
+#define	FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+
+//定义程序中用到的端口
+#define SERV_PORT 9877
+
+
+/* Following shortens all the typecasts of pointer arguments: */
+#define	SA	struct sockaddr
+
+/* prototypes for our socket wrapper functions: see {Sec errors} */
+int		 Accept(int, SA*, socklen_t*);
+void	 Bind(int, const SA*, socklen_t);
+void	 Connect(int, const SA*, socklen_t);
+void	 Getpeername(int, SA*, socklen_t*);
+void	 Getsockname(int, SA*, socklen_t*);
+void	 Getsockopt(int, int, int, void*, socklen_t*);
 
 
 
@@ -16,9 +40,7 @@
 
 
 
-
-
-
+void	 Socketpair(int, int, int, int*);
 
 
 
---
+++ Linux-Socket-ori-Cmake/tcpcliserv/tcpserv01.c
@@ -0,0 +1,27 @@
+#include"unp.h"
+
+int main(int argc ,char **argv){
+	int				listenfd, connfd;
+	pid_t			childpid;
+	socklen_t		clilen;
+
+	struct  sockaddr_in cliaddr, servaddr;
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+	Bind(listenfd, (SA*)&servaddr, sizeof(servaddr));
+	Listen(listenfd, LISTENQ);
+	for (;;) {
+		clilen = sizeof(cliaddr);
+		connfd = Accept(listenfd, (SA*)&cliaddr, &clilen);
+		if ((childpid = Fork()) == 0) {   //进入子进程
+			Close(listenfd);              //关闭listenfd套接字
+			str_echo(connfd);
+			exit(0);
+		}
+		Close(connfd);                   //父进程关闭connfd套接字
+
+	}
+}
